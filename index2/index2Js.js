03413bbbf42945f06cdf66055c64b611c8d24d44
yanis26x <yanis.djenadi@hotmail.com>
2025-02-16 20:55:46 -0500

    idk yea

--- index2/index2Js.js
+++ index2/index2Js.js
@@ -1,8 +1,7 @@
+//opacite du sang!
 document.getElementById('opacitySlider').addEventListener('input', function() {
     document.getElementById('bloodImage').style.opacity = this.value;
 });
-
-
 
 const slider = document.getElementById('opacitySlider');
         const image = document.getElementById('bloodImage');
@@ -10,3 +9,13 @@ const slider = document.getElementById('opacitySlider');
         slider.addEventListener('input', () => {
             image.style.opacity = slider.value;
         });
+
+
+
+let images = ["/yanis26x/OST_IMG/sillentHill.jpg", "/yanis26x/OST_IMG/xp.jpg"]; 
+    let index = 0;
+
+    function changerBackground() {
+        index = (index + 1) % images.length; // Passe à l'image suivante
+        document.body.style.backgroundImage = `url('${images[index]}')`;
+    }
